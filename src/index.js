c7a34084242a43a889e403381445d5e8515fd2ec
Kovalenko Makcim <max.kow@yandex.ru>
2021-09-22 19:32:43 +0300

    дз 7
    подготовка перед кодом

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { PersistGate } from "redux-persist/integration/react";
+
 import "./index.module.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -9,10 +9,8 @@ import App from "./Components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store.store}>
-      <PersistGate loading={null} persistor={store.persistor}>
-        <App />
-      </PersistGate>
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,46 +1,15 @@
-import {
-  configureStore,
-  combineReducers,
-  getDefaultMiddleware,
-} from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import reducersPhoneBook from "./redusers";
 
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-];
-
-const phoneBookPersistConfig = {
-  key: "phoneBook",
-  storage,
-  blacklist: "filters",
-};
-
 const mainReducer = combineReducers({
-  phoneBook: persistReducer(phoneBookPersistConfig, reducersPhoneBook),
+  phoneBook: reducersPhoneBook,
 });
 
 const store = configureStore({
   reducer: mainReducer,
-  middleware,
+
   devTools: process.env.NODE_ENV === "development",
 });
 
-const persistor = persistStore(store);
-
-export default { store, persistor };
+export default store;
